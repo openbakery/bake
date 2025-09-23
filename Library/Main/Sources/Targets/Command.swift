223df87019202b42151a507637c7fb7faa24b220
René Pirringer <rene@pirringer.at>
2025-09-23 17:43:49 +0200

    added debug print

--- Library/Main/Sources/Targets/Command.swift
+++ Library/Main/Sources/Targets/Command.swift
@@ -58,6 +58,7 @@ public class Command: Target, CustomStringConvertible {
 				Task {
 					for line in string.split(separator: "\n") {
 						print(line)
+						print("has outputHander \(outputHandler)")
 						await outputHandler.process(line: String(line))
 					}
 				}
