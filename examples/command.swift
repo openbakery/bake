8dbcf95bb9c93f5af731ed392488816177bdde03
René Pirringer <rene@pirringer.at>
2025-11-30 11:29:09 +0100

    added example

---
+++ examples/command.swift
@@ -0,0 +1,18 @@
+#!/usr/bin/swift sh
+import Bake
+import BakePlugins  // openbakery/Bake == main
+import Foundation
+
+Log.level = .debug
+Log.instance.showLevel = true
+Log.debug("debug")
+
+let commandRunner = await CommandRunner()
+let outputHandler = StringOutputHandler()
+do {
+	await try commandRunner.run("/bin/ls", arguments: ["-la"], outputHandler: outputHandler)
+} catch {
+	// ignore
+}
+
+print("\(outputHandler.lines.joined(separator: "\n"))")
