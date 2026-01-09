7aa561e1f57d3894a2e7f492c39e88908ce56cd7
René Pirringer <rene@pirringer.at>
2026-01-09 18:25:36 +0100

    updated that the bootstrap calls the generated bake script in the end.
    Also hide unnecessary output

--- bake.sh
+++ bake.sh
@@ -1,21 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-WORKING_DIRECTORY=./build/bake/cli
-BAKE_CLI=$WORKING_DIRECTORY/bakeCLI
+WORKING_DIRECTORY=./build/bake
+BAKE_BOOTSTRAP_DIRECTORY=$WORKING_DIRECTORY/cli
+BAKE_BOOTSTRAP_CLI=$BAKE_BOOTSTRAP_DIRECTORY/BakeCLI
+BAKE_CLI=$WORKING_DIRECTORY/bootstrap/bake
 BRANCH=develop
 
+progress() {
+	echo -ne "$1\033[0K\r"
+}
+
 bootstrap() {
+	progress "Bootstrap"
 	if [ -d "$WORKING_DIRECTORY" ]; then
 		rm -rf "$WORKING_DIRECTORY"
 	fi
 
-	mkdir -p $WORKING_DIRECTORY
-	SOURCE_DIRECTORY=$WORKING_DIRECTORY/checkout
-	git clone -b $BRANCH https://github.com/openbakery/bake.git $SOURCE_DIRECTORY
+	mkdir -p $BAKE_BOOTSTRAP_DIRECTORY
+	SOURCE_DIRECTORY=$BAKE_BOOTSTRAP_DIRECTORY/checkout
+	progress "Clone"
+	git clone --quiet -b $BRANCH https://github.com/openbakery/bake.git $SOURCE_DIRECTORY
 
-	swift build -c release --package-path $SOURCE_DIRECTORY
+	progress "Build"
+	swift build --quiet -c release --package-path $SOURCE_DIRECTORY
 
-	mv $SOURCE_DIRECTORY/.build/arm64-apple-macosx/release/bakeCLI $WORKING_DIRECTORY
+	mv $SOURCE_DIRECTORY/.build/arm64-apple-macosx/release/BakeCLI $BAKE_BOOTSTRAP_CLI
+
+	progress "Bootstrap"
+	$BAKE_BOOTSTRAP_CLI bootstrap > /dev/null
 }
 
 parameters=()
@@ -28,15 +40,12 @@ for parameter in "$@"; do
     fi
 done
 
-
-# if [ $# -ge 1 ]; then
-# 	if [ $1 = "--clean" ]; then
-# 		rm -rf "$WORKING_DIRECTORY"
-# 	fi
-# fi
-
 if [ ! -f "$BAKE_CLI" ]; then
+	bootstrap
+elif [ ! -f "$BAKE_BOOTSTRAP_CLI" ]; then
 	bootstrap
 fi
 
+
 $BAKE_CLI "${parameters[@]}"
+
