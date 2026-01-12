fa9450edf2fd03609117df9b4acee63a7eda7f2b
René Pirringer <rene@pirringer.at>
2026-01-12 14:09:20 +0100

    added teamcity config

---
+++ .teamcity/settings.kts
@@ -0,0 +1,40 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+version = "2025.03"
+
+
+project {
+
+	val build = Build()
+
+}
+
+class Build : BuildType({
+	name = "Build And Test"
+
+	vcs {
+		root(DslContext.settingsRoot)
+	}
+
+	steps {
+		exec {
+			path = "/usr/bin/swift"
+			arguments = "test"
+		}
+	}
+
+	features {
+		swabra { }
+	}
+
+	triggers {
+		vcs {
+		}
+	}
+})
+
